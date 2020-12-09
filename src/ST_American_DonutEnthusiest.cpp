1f514065a97b67db5ac17e27a165c64d6731ae99
danny <danny@danny-VirtualBox>
2020-12-08 22:44:06 -0500

    donut

--- src/ST_American_DonutEnthusiest.cpp
+++ src/ST_American_DonutEnthusiest.cpp
@@ -1,2 +1,16 @@
-#include "ST_American_DonutEnthusiest"
+#include <string>
+#include "../includes/Smalltalk.h"
+#include "../includes/ST_American_DonutEnthusiest.h"
+#include "../includes/Smalltalk_American.h"
+#include "../includes/constants.h"
+ST_American_DonutEnthusiest::ST_American_DonutEnthusiest(int iPerson):Smalltalk_American(AMERICAN_DE,iPerson){
+	ST_American_DonutEnthusiest::populatePhrases();
+}
 
+void ST_American_DonutEnthusiest::populatePhrases(){
+	Smalltalk::mySmallTalk.push_back(AMERICAN_DE_PHRASE_1);
+	Smalltalk::mySmallTalk.push_back(AMERICAN_DE_PHRASE_2);
+	Smalltalk::mySmallTalk.push_back(AMERICAN_DE_PHRASE_3);
+	Smalltalk::mySmallTalk.push_back(AMERICAN_DE_PHRASE_4);
+	Smalltalk::mySmallTalk.push_back(AMERICAN_DE_PHRASE_5);
+}
