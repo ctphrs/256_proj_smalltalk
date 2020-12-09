f17f91b1c5cdf7b7d2b4e0a8bce6500466bef0b9
danny <danny@danny-VirtualBox>
2020-12-08 21:18:51 -0500

    start

--- src/Smalltalk.cpp
+++ src/Smalltalk.cpp
@@ -1,3 +1,15 @@
-/*
- * TODO
- */
+#include <string>
+#include <vector>
+#include <memory>
+
+#include "../includes/Smalltalk.h"
+
+using namespace std;
+
+Smalltalk::Smalltalk(std::string myNationality,int iPerson=1): nationality(myNationality), iPerson(iPerson), current_phrase(0){
+
+}
+
+string Smalltalk::saySomething(){
+
+}
